4a067ed0b4d97161b5b817d70df01a57c3061a63
Cristian Oliveira da Rosa <contato@cristianoliveira.com.br>
2015-11-07 01:14:34 -0200

    chore: moving parser build for its own class

--- croutera.py
+++ croutera.py
@@ -1,41 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from croutera.cli import Cli
 import argparse
+from croutera.cli import Cli, ParserBuilder
 
 def main():
     print("Croutera...")
-
-    description = 'Simple terminal cli to manage modem / routers admin actions'
-
-    parser = argparse.ArgumentParser(description = description,
-                                     argument_default=argparse.SUPPRESS)
-
-    parser.add_argument(
-        'model',
-        nargs = '?',help = 'Router model (see: --list-model)')
-
-    parser.add_argument(
-        'username',
-        nargs = '?', help = 'User name to access admin page')
-    parser.add_argument(
-        'password',
-        nargs = '?',help = 'Password to access admin page')
-
-    parser.add_argument(
-        '-restart', dest = 'restart',
-        action = 'store_true', default=False,
-        help = 'Reset router by model.'
-    )
-
-    parser.add_argument(
-        '-list-models', dest = 'list_models',
-        action = 'store_true',  default=False,
-        help = 'Shows models available'
-    )
-
-    args = parser.parse_args()
+    args = ParserBuilder.build().parse_args()
     Cli.command(args).execute()
 
 if __name__ == '__main__':
--- croutera/cli.py
+++ croutera/cli.py
@@ -5,8 +5,46 @@
   Cli terminal module
 """
 
+import argparse
+
 from models.routers import Routers
 from commands import ModelListCommand, RestartCommand
+
+class ParserBuilder:
+
+    @staticmethod
+    def build():
+        description = 'Simple terminal cli to manage modem ' \
+                                      '/ routers admin actions'
+
+        parser = argparse.ArgumentParser(description = description,
+                                         argument_default=argparse.SUPPRESS)
+
+        parser.add_argument(
+            'model',
+            nargs = '?',help = 'Router model (see: --list-model)')
+
+        parser.add_argument(
+            'username',
+            nargs = '?', help = 'User name to access admin page')
+        parser.add_argument(
+            'password',
+            nargs = '?',help = 'Password to access admin page')
+
+        parser.add_argument(
+            '-restart', dest = 'restart',
+            action = 'store_true', default=False,
+            help = 'Reset router by model.'
+        )
+
+        parser.add_argument(
+            '-list-models', dest = 'list_models',
+            action = 'store_true',  default=False,
+            help = 'Shows models available'
+        )
+
+        return parser
+
 
 class Cli(object):
 
