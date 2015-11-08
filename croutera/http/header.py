b58a2be605f22895bcc56385923609d28c28f397
Cristian Oliveira da Rosa <contato@cristianoliveira.com.br>
2015-11-08 05:16:20 -0200

    refact: adding more meaning

--- croutera/http/header.py
+++ croutera/http/header.py
@@ -7,7 +7,7 @@ import base64
 class Header(object):
 
     @staticmethod
-    def prepare_base64_auth(username, password):
+    def basic_base64_auth(username, password):
         auth_string = '%s:%s' % (username, password)
 
         if sys.version_info[0] == 3:
@@ -17,5 +17,5 @@ class Header(object):
 
         if sys.version_info[0] == 3:
             encoded = encoded.decode('ascii')
-        return encoded
+        return "Basic %s" % encoded
 
