db993a06b6b015eeafe184c21f89b38c2480b979
Cristian Oliveira da Rosa <contato@cristianoliveira.com.br>
2015-11-11 22:06:34 -0200

    tests: adding tests for Routers

--- croutera/models/routers.py
+++ croutera/models/routers.py
@@ -10,19 +10,38 @@ import base64
 
 from croutera.models.tplink import *
 from croutera.models.dlink import *
+from croutera.exceptions import ModelNotFoundError
 
 class Routers(object):
     """ Provide Router.class instances """
 
-    MODELS = {
-        'dlink-dir610' : DLinkDir610,
-        'tplink-wr340g' : TplinkWR340
+    MANUFACTURER_MODELS = {
+        'dlink' : {
+            'dir610' : DLinkDir610
+        },
+        'tplink' : {
+            'wr340g' : TplinkWR340,
+            'wr720n' : TplinkWR720N
+        }
     }
 
     @staticmethod
-    def get(model):
-        model = Routers.MODELS.get(model)
-        return model()
+    def get(manufacturer, model):
+        router = Routers.MANUFACTURER_MODELS.get(manufacturer).get(model)
+
+        if router is None:
+            raise ModelNotFoundError('Model not found for this manufacturer.')
+
+        return router
+
+    @staticmethod
+    def from_manufacturer(manufacturer):
+        models = Routers.MANUFACTURER_MODELS.get(manufacturer)
+
+        if models is None:
+            raise ModelNotFoundError('Model not found for this manufacturer.')
+
+        return models
 
     @staticmethod
     def list():
---
+++ tests/models/test_router.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+  Tests for Cli module
+"""
+
+import unittest
+import argparse
+
+import mock
+from croutera.models.routers import Routers
+from croutera.exceptions import ModelNotFoundError
+
+class RoutersTest(unittest.TestCase):
+
+    def setUp(self):
+        self.model1 = mock.Mock({'description' : 'manufacturer model 1'})
+        self.model2 = mock.Mock({'description' : 'manufacturer model 2'})
+        self.model3 = mock.Mock({'description' : 'manufacturer2 model 1'})
+        self.model4 = mock.Mock({'description' : 'manufacturer2 model 2'})
+        self.models = {
+           'manufacturer' : {
+                'model1' : self.model1,
+                'model2' : self.model2
+           },
+           'manufacturer2' : {
+                'model3' : self.model3,
+                'model4' : self.model4
+           }
+        }
+        Routers.MANUFACTURER_MODELS = self.models
+
+    def test_it_returns_all_models_from_manufacturer(self):
+        self.assertEqual(self.models.get('manufacturer'),
+                         Routers.from_manufacturer('manufacturer'))
+
+    def test_it_returns_model_from_manufacturer(self):
+        self.assertEqual(self.model1, Routers.get('manufacturer', 'model1'))
+
+    def test_it_raise_model_not_found_error(self):
+        self.assertRaises(ModelNotFoundError, Routers.get, 'manufacturer', 'model_invalid')
