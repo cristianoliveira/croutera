2aaf81a10e252280f61d21db681c842bb046c838
Cristian Oliveira <contato@cristianoliveira.com.br>
2015-11-15 12:03:40 -0200

    test: tests for ParserBuilder

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -6,27 +6,26 @@
 """
 
 import unittest
-import argparse
 
-from mock import Mock, patch, call
+from mock import Mock, patch
 from croutera import runner
+
 
 class RunnerTest(unittest.TestCase):
 
     @patch('croutera.runner.ParserBuilder')
     def test_it_runs_with_no_argument(self, ParserBuilder):
         args = Mock()
-        parser = Mock(**{'parse_args' : args})
+        parser = Mock(**{'parse_args': args})
         ParserBuilder.build.return_value = parser
 
         self.assertTrue(runner.run())
 
-
     @patch('croutera.runner.Cli')
     @patch('croutera.runner.ParserBuilder')
-    def test_it_shows_help_whit_no_args(self, ParserBuilder, Cli):
+    def test_it_shows_help_when_doesnt_found_command(self, ParserBuilder, Cli):
         Cli.command.return_value = None
 
         runner.run()
 
-        ParserBuilder.build().parse_args.assert_called_with(['-h'])
+        ParserBuilder.build_help.assert_called_once()
